52a02b9dd26dc46287320cbe8331789826b8d1ff
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-07-14 13:26:27 +0300

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,8 +1,28 @@
+const blog = require("../models/blog")
 
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    
+    else if (blogs.length === 1) {
+        return blogs[0].likes
+    }
+
+    else {
+    const initialValue = 0;
+    const likes = blogs.map(blog => blog.likes)
+    const total = likes.reduce((num, nextNum) => num + nextNum, initialValue);
+    return total
+
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
