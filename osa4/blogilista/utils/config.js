57e130d1d3c90fa719765ad1650b3ad1e603a48b
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-07-19 11:19:53 +0300

    päöivitänytsevitunportoikeeseenmiljoonattakertaa

--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.MONGODB_URI
+const PORT = process.env.PORT
 
 const MONGODB_URI = process.env.PORT.NODE_ENV === 'test'
 ? process.env.TEST_MOLNGODB_URI
