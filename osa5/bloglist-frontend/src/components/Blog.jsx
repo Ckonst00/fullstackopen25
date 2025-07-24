8136632e1701f8ede2c3564f660f6cfe491dc16c
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-07-24 11:17:12 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const informationVisible = () => {
+    
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    return (
+      <div style={blogStyle}>
+        <div style={hideWhenVisible}>
+         <div>{blog.title} {blog.author} <button onClick={() => setVisible(true)}>view</button></div>
+        </div>
+        <div style={showWhenVisible}>
+          <div>{blog.title} {blog.author} <button onClick={() => setVisible(false)}>hide</button></div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}<button>like</button></div>
+          <div>{blog.user?.name || "Unknown user"}</div>
+        </div>
+      </div>
+
+    )
+  }
+
+
+  return (
+    <div>
+      {informationVisible()}
+    </div>  
+  );
+};
+
+export default Blog;
