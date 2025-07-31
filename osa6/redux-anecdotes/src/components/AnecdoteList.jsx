7db0571837a694a50ec62c785e7980f3c5e48fdf
Aleksi Kaurila <121864367+Ckonst00@users.noreply.github.com>
2025-07-31 15:41:50 +0300

    6.8

---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux"
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+
+    const dispatch = useDispatch()
+    const anecdotes = useSelector(state => state)
+
+    const setVote = (id) => {
+    dispatch(vote(id))
+    }
+
+    return (
+        <div>
+        {anecdotes
+            .sort((a, b) => b.votes - a.votes)
+            .map(anecdote =>
+            <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => setVote(anecdote.id)}>vote</button>
+            </div>
+            </div>
+        )}
+        </div>
+    )
+}
+
+export default AnecdoteList
